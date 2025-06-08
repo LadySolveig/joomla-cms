7525f74a6c2e599cdf33eebd44ba28896d07c32e
Martina Scholz <martina@simplysmart-it.de>
2025-06-08 21:37:26 +0200

    Add reload method to FormController

--- administrator/components/com_formbuilder/src/Controller/FormController.php
+++ administrator/components/com_formbuilder/src/Controller/FormController.php
@@ -102,4 +102,19 @@ class FormController extends LibFormController
         }
         $this->app->close();
     }
+
+    /**
+     * Method to reload a record.
+     *
+     * @param   string  $key     The name of the primary key of the URL variable.
+     * @param   string  $urlVar  The name of the URL variable if different from the primary key (sometimes required to avoid router collisions).
+     *
+     * @return  void
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function reload($key = null, $urlVar = 'id')
+    {
+        parent::reload($key, $urlVar);
+    }
 }
