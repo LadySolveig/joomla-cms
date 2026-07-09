e88be70f5257e98be7bca5e6321db3e34e1f17d0
Martina Scholz <martina.scholz@simplysmart-it.de>
2026-07-09 11:17:29 +0200

    Refactor integration tests for article options hierarchy to use 'show_create_date' instead of 'show_author'

--- tests/System/integration/site/components/com_content/ArticleOptionsHierarchy.cy.js
+++ tests/System/integration/site/components/com_content/ArticleOptionsHierarchy.cy.js
@@ -3,8 +3,9 @@ describe('Test in frontend that the content article view honours the option hier
   // global < menu item < article. In addition a menu item option set to
   // 'use_article' defers to the article option and falls back to the global
   // value when the article is set to "Use Global".
-  // We use show_author as a representative option: it renders the byline
-  // (<dd class="createdby">Written by ...</dd>) only when it evaluates to true.
+  // We use show_create_date as a representative option: it renders the created
+  // date (<dd class="create">Created: ...</dd>) whenever it evaluates to true,
+  // gated only by the option itself (no extra data dependency).
 
   const menuItemTitle = 'automated test article options';
 
@@ -12,16 +13,13 @@ describe('Test in frontend that the content article view honours the option hier
     cy.db_deleteMenuItem({ title: menuItemTitle });
     cy.task('queryDB', "DELETE FROM #__content WHERE title = 'test article'");
     // Restore the global option to its default so tests do not leak state.
-    cy.db_updateExtensionParameter('show_author', '1', 'com_content');
+    cy.db_updateExtensionParameter('show_create_date', '1', 'com_content');
   });
 
-  const assertBylineShown = () => cy.get('.createdby').should('contain', 'Test Author');
-  const assertBylineHidden = () => cy.get('.createdby').should('not.exist');
+  const assertDateShown = () => cy.get('.create').should('exist');
+  const assertDateHidden = () => cy.get('.create').should('not.exist');
 
-  const createArticle = (attribs) => cy.db_createArticle({
-    created_by_alias: 'Test Author',
-    attribs,
-  });
+  const createArticle = (attribs) => cy.db_createArticle({ attribs });
 
   const createMenuItem = (article, params) => cy.db_createMenuItem({
     title: menuItemTitle,
@@ -33,56 +31,56 @@ describe('Test in frontend that the content article view honours the option hier
 
   describe('with a menu item whose option is set to use_article', () => {
     it('uses the article option over the global one (show)', () => {
-      cy.db_updateExtensionParameter('show_author', '0', 'com_content')
-        .then(() => createArticle('{"show_author":"1"}'))
-        .then((article) => createMenuItem(article, '{"show_author":"use_article"}')
+      cy.db_updateExtensionParameter('show_create_date', '0', 'com_content')
+        .then(() => createArticle('{"show_create_date":"1"}'))
+        .then((article) => createMenuItem(article, '{"show_create_date":"use_article"}')
           .then((menuId) => {
             cy.visit(`/index.php?option=com_content&view=article&id=${article.id}&catid=${article.catid}&Itemid=${menuId}`);
-            assertBylineShown();
+            assertDateShown();
             cy.checkForPhpNoticesOrWarnings();
           }));
     });
 
     it('uses the article option over the global one (hide)', () => {
-      cy.db_updateExtensionParameter('show_author', '1', 'com_content')
-        .then(() => createArticle('{"show_author":"0"}'))
-        .then((article) => createMenuItem(article, '{"show_author":"use_article"}')
+      cy.db_updateExtensionParameter('show_create_date', '1', 'com_content')
+        .then(() => createArticle('{"show_create_date":"0"}'))
+        .then((article) => createMenuItem(article, '{"show_create_date":"use_article"}')
           .then((menuId) => {
             cy.visit(`/index.php?option=com_content&view=article&id=${article.id}&catid=${article.catid}&Itemid=${menuId}`);
-            assertBylineHidden();
+            assertDateHidden();
             cy.checkForPhpNoticesOrWarnings();
           }));
     });
 
     it('falls back to the global option when the article is set to use global', () => {
-      cy.db_updateExtensionParameter('show_author', '1', 'com_content')
+      cy.db_updateExtensionParameter('show_create_date', '1', 'com_content')
         .then(() => createArticle(''))
-        .then((article) => createMenuItem(article, '{"show_author":"use_article"}')
+        .then((article) => createMenuItem(article, '{"show_create_date":"use_article"}')
           .then((menuId) => {
             cy.visit(`/index.php?option=com_content&view=article&id=${article.id}&catid=${article.catid}&Itemid=${menuId}`);
-            assertBylineShown();
+            assertDateShown();
             cy.checkForPhpNoticesOrWarnings();
           }));
     });
 
     it('falls back to the global option (hide) when the article is set to use global', () => {
-      cy.db_updateExtensionParameter('show_author', '0', 'com_content')
+      cy.db_updateExtensionParameter('show_create_date', '0', 'com_content')
         .then(() => createArticle(''))
-        .then((article) => createMenuItem(article, '{"show_author":"use_article"}')
+        .then((article) => createMenuItem(article, '{"show_create_date":"use_article"}')
           .then((menuId) => {
             cy.visit(`/index.php?option=com_content&view=article&id=${article.id}&catid=${article.catid}&Itemid=${menuId}`);
-            assertBylineHidden();
+            assertDateHidden();
             cy.checkForPhpNoticesOrWarnings();
           }));
     });
 
     it('lets an explicit article option override an explicit menu item option', () => {
-      cy.db_updateExtensionParameter('show_author', '1', 'com_content')
-        .then(() => createArticle('{"show_author":"1"}'))
-        .then((article) => createMenuItem(article, '{"show_author":"0"}')
+      cy.db_updateExtensionParameter('show_create_date', '1', 'com_content')
+        .then(() => createArticle('{"show_create_date":"1"}'))
+        .then((article) => createMenuItem(article, '{"show_create_date":"0"}')
           .then((menuId) => {
             cy.visit(`/index.php?option=com_content&view=article&id=${article.id}&catid=${article.catid}&Itemid=${menuId}`);
-            assertBylineShown();
+            assertDateShown();
             cy.checkForPhpNoticesOrWarnings();
           }));
     });
@@ -90,31 +88,31 @@ describe('Test in frontend that the content article view honours the option hier
 
   describe('with direct access and no menu item assigned', () => {
     it('applies the article option over the global one (hide)', () => {
-      cy.db_updateExtensionParameter('show_author', '1', 'com_content')
-        .then(() => createArticle('{"show_author":"0"}'))
+      cy.db_updateExtensionParameter('show_create_date', '1', 'com_content')
+        .then(() => createArticle('{"show_create_date":"0"}'))
         .then((article) => {
           cy.visit(`/index.php?option=com_content&view=article&id=${article.id}&catid=${article.catid}`);
-          assertBylineHidden();
+          assertDateHidden();
           cy.checkForPhpNoticesOrWarnings();
         });
     });
 
     it('applies the article option over the global one (show)', () => {
-      cy.db_updateExtensionParameter('show_author', '0', 'com_content')
-        .then(() => createArticle('{"show_author":"1"}'))
+      cy.db_updateExtensionParameter('show_create_date', '0', 'com_content')
+        .then(() => createArticle('{"show_create_date":"1"}'))
         .then((article) => {
           cy.visit(`/index.php?option=com_content&view=article&id=${article.id}&catid=${article.catid}`);
-          assertBylineShown();
+          assertDateShown();
           cy.checkForPhpNoticesOrWarnings();
         });
     });
 
     it('honours the global option when the article is set to use global', () => {
-      cy.db_updateExtensionParameter('show_author', '0', 'com_content')
+      cy.db_updateExtensionParameter('show_create_date', '0', 'com_content')
         .then(() => createArticle(''))
         .then((article) => {
           cy.visit(`/index.php?option=com_content&view=article&id=${article.id}&catid=${article.catid}`);
-          assertBylineHidden();
+          assertDateHidden();
           cy.checkForPhpNoticesOrWarnings();
         });
     });
